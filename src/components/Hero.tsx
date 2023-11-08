aff337cdfafe80e11b9c1a20a0527a4c9a56f90f
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-08 14:49:22 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,6 +7,19 @@ const Hero = () => {
     <div className='relative min-h-screen'>
         <Image className='lg:w-[580px] xl:w-[620px] h-auto absolute top-0 right-0 -z-10' src='/hero2.png' alt='hero bg' width={1000} height={600} />
         <Navbar />
+        <div className='container h-[calc(100vh-120px)] grid items-center'>
+            <div className='space-y-4 bg-[#ffffff98] w-fit p-4'>
+                <p className='uppercase'>Wide Options of Choice</p>
+                <h2 className='text-4xl sm:text-6xl font-bold'>
+                    Delicious <span className='text-accent'>Food</span>
+                </h2>
+                <p className='text-gray-700 text-[14px] sm:text-[16px]'>
+                    Delicious food color, aroma and taste . <br />
+                    What are you wainting for?
+                </p>
+                <button className='bg-accent text-white px-6 py-4 rounded-3xl text-[14px] sm:text-[16px]'>View More </button>
+            </div>
+        </div>
     </div>
   )
 }
