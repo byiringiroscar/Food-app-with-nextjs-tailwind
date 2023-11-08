522fe75016076e67fe5d3ab4e3af0e48c24572cd
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-08 13:59:26 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>Hello</main>
+    <main>
+      <Hero />
+    </main>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='relative min-h-screen'></div>
+    <div className='relative min-h-screen'>
+        <Image src='/hero2.png' alt='hero bg' width={1000} height={600} />
+    </div>
   )
 }
 
