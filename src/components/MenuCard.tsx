9fad0607ffc6d90ad1fe443578c2d2a6c36ff048
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-08 18:14:08 +0200

    Add menu card

---
+++ src/components/MenuCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Image from "next/image";
+
+
+interface propsType {
+    img: string;
+    title: string;
+    desc: string;
+    price: string;
+  }
+
+  const MenuCard: React.FC<propsType> = ({ img, title, desc, price }) => {
+  return (
+    <div className="flex gap-2">
+      <Image
+        className="w-[80px] h-[80px]"
+        src={img}
+        width={80}
+        height={80}
+        alt="dish"
+      />
+      <div className="space-y-2">
+        <div>
+          <h2>{title}</h2>
+          <p className="text-[14px] text-gray-600 pt-1">{desc}</p>
+        </div>
+
+        <p className="text-accent">{price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default MenuCard
