ecfa3ef1d37037406f740dda242b272ff2c77233
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-08 15:22:26 +0200

    Create Feature.tsx

---
+++ src/components/Feature.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div className='container pt-40'>
+        <h2 className='text-6xl font-bold'>Our</h2>
+        <h2 className='text-6xl font-bold pt-2'>Featured <span className='text-accent'>Food</span></h2>
+        <p className='max-w-[550px] pt-10 text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, quos?</p>
+    </div>
+  )
+}
+
+export default Feature
