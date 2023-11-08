ac94ac009c1a7594c6bd8eb22aef2b41df01402a
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-11-08 15:25:31 +0200

    wip

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Dash from './Dash'
 
 const Feature = () => {
   return (
@@ -6,6 +7,8 @@ const Feature = () => {
         <h2 className='text-6xl font-bold'>Our</h2>
         <h2 className='text-6xl font-bold pt-2'>Featured <span className='text-accent'>Food</span></h2>
         <p className='max-w-[550px] pt-10 text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, quos?</p>
+
+        <Dash />
     </div>
   )
 }
